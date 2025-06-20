002e2ed047d49fe515a7a50c2c2ceb8f3a22bf43
orange0414 <xichengyan0414@gmail.com>
2025-06-20 12:03:37 +0200

    update

--- WerewolfManager_v1/main.py
+++ WerewolfManager_v1/main.py
@@ -4,7 +4,7 @@ import copy
 
 
 class Role:
-    def __init__(self, name, team, description):
+    def __init__(self, name:str, team:str, description:str):
         self.name = name
         self.team = team
         self.description = description
@@ -18,11 +18,24 @@ class Role:
     def toString(self):
         return f"【{self.name}】【{self.team}】: \n {self.description}"
 
+class Player:
+    def __init__(self, id:int, role:Role):
+        self.id = id
+        self.role = role
+        self.alive = True
+        self.death_type:str = None
+    
+    def isAlive(self):
+        return self.alive
+    
+    def deathType(self):
+        return self.death_type
+
 with open("data/roles.json", "r", encoding = "utf-8") as f:
     data = json.load(f)
 
 allRoles = [Role(**item) for item in data]
-actualRoles = []
+currentRoles = []
 currentBoard = ""
 
 '''
@@ -55,13 +68,13 @@ def main():
             
             match action:
                 case 0:
-                    msg = "感谢您的支持！欢迎下次使用！"
-                    hotpink = (255, 105, 180)
-                    aqua    = (  0, 255, 255)
-                    print_gradient_text(msg, hotpink, aqua)
+                    msg = "\n【感谢您的支持！欢迎下次使用！】\n"
+                    startColor = (54, 209, 220)
+                    endColor    = (91, 134, 229)
+                    print_gradient_text(msg, startColor, endColor)
                     return
                 case 1:
-                    showRoles(actualRoles)
+                    showRoles(currentRoles)
                     break
                 case 2:
                     showRoles(allRoles)
@@ -80,7 +93,7 @@ def showRoles(roles: list[Role]):
     
     if roles:
         print("\033[38;2;255;105;180m" + "="*178)
-        title = f"【<{currentBoard}>角色介绍】" if roles == actualRoles else "【<所有版型>角色介绍】"
+        title = f"【<{currentBoard}>角色介绍】" if roles == currentRoles else "【<所有版型>角色介绍】"
         print(title + "\033[0m")
     
         # 定义阵营排序权重
@@ -107,14 +120,14 @@ def showRoles(roles: list[Role]):
                 print(f"{color}【{role.name}】({role.team})\n\033[0m{role.description}")
             else:
                 print(role.toString())
-            
+            print()
             print("="*178)
                 
         print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
                 
     elif not roles:
         print("\033[31m" + "="*178 + "\033[0m")
-        print("\033[31m" + "\t【未选择版型】" + "\033[0m")
+        print("\033[31m" + "【未选择版型】" + "\033[0m")
         print("\033[31m" + "="*178 + "\033[0m")
 
 '''
@@ -123,71 +136,196 @@ def showRoles(roles: list[Role]):
 def chooseBoard():
     
     global currentBoard
-    valid_actions = [0, 1]
+    valid_actions = [0, 1, 2, 3, 4, 5, 6, 7]
     
-    while(True):
-        action = None
+    action = None
+    
+    while action not in valid_actions:
+        board_menu()
+        print("="*178)
+        action = input("请输入你的选项: ")
+        print("="*178)
+        try:
+            action = int(action)
+        except ValueError:
+            print("\033[31m" + "="*178 + "\033[0m")
+            print("\033[31m" + "【注意！ 请输入以下选项中的一项: " + f"{valid_actions}" + "】\033[0m")
+            print("\033[31m" + "="*178 + "\033[0m")
+            continue
+        if not action in valid_actions:
+            print("\033[31m" + "="*178 + "\033[0m")
+            print("\033[31m" + "【注意！ 请输入有效选项: " + f"{valid_actions}" + "】\033[0m")
+            print("\033[31m" + "="*178 + "\033[0m")
+            continue
         
-        while action not in [1]:
-            board_menu()
-            print("="*178)
-            action = input("请输入你的选项: ")
-            print("="*178)
-            try:
-                action = int(action)
-            except ValueError:
-                print("\033[31m" + "="*178 + "\033[0m")
-                print("\033[31m" + "【注意！ 请输入以下选项中的一项: " + f"{valid_actions}" + "】\033[0m")
-                print("\033[31m" + "="*178 + "\033[0m")
-                continue
-            if not action in valid_actions:
-                print("\033[31m" + "="*178 + "\033[0m")
-                print("\033[31m" + "【注意！ 请输入有效选项: " + f"{valid_actions}" + "】\033[0m")
-                print("\033[31m" + "="*178 + "\033[0m")
-                continue
-            
-            match action:
-                case 0:
-                    return
-                case 1:
-                    currentBoard = "预女猎白混"
-                    pickRoles({"预言家":1, "女巫":1, "猎人":1, "白痴":1, "混子":1, "村民":3, "狼人":4})
-                    print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
-                    print("\033[32m" + "\t您已选择【预女猎白混】配置" + "\033[0m")
-                    print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
-                case 2:
-                    pass
-                case 3:
-                    pass
-                case 4:
-                    pass
+        match action:
+            case 0:
+                break
+            case 1:
+                currentBoard = "预女猎白混"
+                pickRoles({"预言家":1, "女巫":1, "猎人":1, "白痴":1, "混子":1, "村民":3, "狼人":4})
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                print("\033[32m" + "【您已选择<预女猎白混>配置】" + "\033[0m")
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                break
+            case 2:
+                currentBoard = "假面舞会"
+                pickRoles({"预言家":1, "女巫":1, "舞者":1, "白痴":1, "村民":4, "狼人":3, "假面":1})
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                print("\033[32m" + "【您已选择<假面舞会>配置】" + "\033[0m")
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                break
+            case 3:
+                currentBoard = "孤注一掷"
+                pickRoles({"预言家":1, "女巫":1, "猎人":1, "摄梦人":1, "村民":4, "狼人":3, "典狱长":1})
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                print("\033[32m" + "【您已选择<孤注一掷>配置】" + "\033[0m")
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                break
+            case 4:
+                currentBoard = "机械狼通灵师"
+                pickRoles({"通灵师":1, "女巫":1, "守卫":1, "猎人":1, "村民":4, "狼人":3, "机械狼":1})
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                print("\033[32m" + "【您已选择<机械狼通灵师>配置】" + "\033[0m")
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                break
+            case 5:
+                currentBoard = "盗宝通灵"
+                pickRoles({"通灵师":1, "毒师":1, "猎人":1, "摄梦人":1, "蒙面人":1,"村民":5, "狼人":4, "盗宝大师":1})
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                print("\033[32m" + "【您已选择<盗宝通灵>配置】" + "\033[0m")
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                break
+            case 6:
+                currentBoard = "鬼魂新娘"
+                pickRoles({"预言家":1, "女巫":1, "猎人":1, "守卫":1, "村民":4, "狼人":3, "鬼魂新娘":1})
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                print("\033[32m" + "【您已选择<鬼魂新娘>配置】" + "\033[0m")
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                break
+            case 7:
+                currentBoard = "骑士狼美"
+                pickRoles({"预言家":1, "女巫":1, "守卫":1, "骑士":1, "村民":4, "狼人":3, "狼美人":1})
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                print("\033[32m" + "【您已选择<骑士狼美>配置】" + "\033[0m")
+                print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+                break
+        return
 
 def pickRoles(rolesToPick:dict[str,int]):
-    global actualRoles
-    actualRoles = []
+    global currentRoles
+    currentRoles = []
     for name, cnt in rolesToPick.items():
         # 在 allRoles 里找到这个 name 对应的 Role 模板
         template = next((r for r in allRoles if r.name == name), None)
         if not template:
-            print("\033[31m" + "="*178 + "\033[0m")
-            print(f"\t【未能找到角色】: {name}")
-            print("\033[31m" + "="*178 + "\033[0m")
-            return
+            print("\033[31m" + "="*178)
+            print(f"【未能在现有角色中找到角色】: {name}")
+            print("="*178 + "\033[0m")
         # 拷贝
         for _ in range(cnt):
-            actualRoles.append(copy.deepcopy(template))
+            currentRoles.append(copy.deepcopy(template))
 
 '''
 ----------------------------- Start Game -----------------------------------
 '''
 def startGame():
-    if not actualRoles or currentBoard == "":
+    if not currentRoles or currentBoard == "":
         print("\033[31m" + "="*178 + "\033[0m")
         print("\033[31m" + "【注意！ 未选择板子或选择出错" + "】\033[0m")
         print("\033[31m" + "="*178 + "\033[0m")
         return
     
+    match currentBoard:
+        case "预女猎白混":
+            print("\033[32m" + "="*178)
+            print(f"【正在编写】")
+            print("="*178 + "\033[0m")
+            startGameBoard1()
+        case _:
+            msg = "\n【尚未加入该模型，尽请期待！】\n"
+            startColor = (54, 209, 220)
+            endColor    = (91, 134, 229)
+            print_gradient_text(msg, startColor, endColor)
+
+# 预女猎白混
+def startGameBoard1():
     
+    print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+    
+    players = create_players_from_roles(currentRoles)
+    
+    print("\033[38;2;100;149;237m" + '='*178 + '\033[0m')
+    showCurrentPlayers(players)
+    print("\033[38;2;100;149;237m" + '='*178 + '\033[0m')
+    
+    print('='*178)
+    print("【请准备好身份牌并分发给每位对应玩家给每位玩家】")
+    print('='*178)
+    
+    DAY = "day"
+    NIGHT = "night"
+    day = 1
+    phase = "night"
+    game_over = False
+    
+    while(not game_over):
+        
+        if phase == NIGHT:
+            nightPhase(day)
+            day += 1
+            phase = DAY
+            
+        elif phase == DAY:
+            dayPhase(day)
+            phase = NIGHT
+    
+    
+    print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+
+def nightPhase(day:int):
+    pass
+
+def dayPhase(day):
+    pass
+
+def showCurrentPlayers(players:list[Player]):
+    print()
+    for p in players:
+        if p.role.team == "神职":
+            color = "\033[38;5;220m"
+        elif p.role.team == "狼人":
+            color = "\033[38;5;196m"
+        elif p.role.name == "混子" or p.role.team == "第三方":
+            color = "\033[38;5;141m"
+        elif p.role.team == "村民":
+            color = "\033[38;5;94m"
+        else:
+            color = ""
+        
+        if p.isAlive():
+            print(f"\t{p.id} 号玩家\t{color}【{p.role.name}】\033[0m")
+        else:
+            print(f"\t\033[9m{p.id} 号玩家\t\033[38;2;211;211;211m【{p.role.name}】\033[0m" + f" {p.deathType()}")
+    print()
+
+    
+def create_players_from_roles(roles: list[Role]) -> list[Player]:
+    """
+    根据给定的 Role 列表，创建对应的 Player 列表。
+    
+    :param roles: 已选定的角色列表 actualRoles
+    :param shuffle_roles: 是否先打乱角色顺序，默认 True
+    :return: Player 对象列表
+    """
+    roles_copy = roles.copy()
+    random.shuffle(roles_copy)
+    
+    players = []
+    for idx, role in enumerate(roles_copy, start=1):
+        players.append(Player(id=idx, role=role))
+    return players
+
 '''
 ------------------------------- Menu --------------------------------------- 
 '''
@@ -210,6 +348,12 @@ def board_menu():
     【板子选择】
     0. 返回
     1. 预女猎白混
+    2. 假面舞会
+    3. 孤注一掷
+    4. 机械狼通灵师
+    5. 盗宝通灵
+    6. 鬼魂新娘
+    7. 骑士狼美
     ''' +"\033[0m")
     print("\033[38;2;255;105;180m"+ "="*178 + "\033[0m")
 
@@ -237,4 +381,8 @@ def print_gradient_text(text: str,
 
 
 if __name__ == "__main__":
+    msg = "\n【欢迎使用WerewolfManager！】\n"
+    hotpink = (255, 105, 180)
+    aqua    = (  0, 255, 255)
+    print_gradient_text(msg, hotpink, aqua)
     main()
