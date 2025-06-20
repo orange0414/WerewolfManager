0a0f5011917177344b57e84975404176347839bd
orange0414 <xichengyan0414@gmail.com>
2025-06-20 19:10:37 +0200

    更新预女猎白混开始游戏(部分)

--- WerewolfManager_v1/main.py
+++ WerewolfManager_v1/main.py
@@ -23,7 +23,7 @@ class Player:
         self.id = id
         self.role = role
         self.alive = True
-        self.death_type:str = None
+        self.death_type:str = ""
     
     def isAlive(self):
         return self.alive
@@ -229,6 +229,7 @@ def pickRoles(rolesToPick:dict[str,int]):
 '''
 ----------------------------- Start Game -----------------------------------
 '''
+
 def startGame():
     if not currentRoles or currentBoard == "":
         print("\033[31m" + "="*178 + "\033[0m")
@@ -241,7 +242,7 @@ def startGame():
             print("\033[32m" + "="*178)
             print(f"【正在编写】")
             print("="*178 + "\033[0m")
-            startGameBoard1()
+            GameBoard1().start()
         case _:
             msg = "\n【尚未加入该模型，尽请期待！】\n"
             startColor = (54, 209, 220)
@@ -249,45 +250,245 @@ def startGame():
             print_gradient_text(msg, startColor, endColor)
 
 # 预女猎白混
-def startGameBoard1():
-    
-    print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
-    
-    players = create_players_from_roles(currentRoles)
-    
-    print("\033[38;2;100;149;237m" + '='*178 + '\033[0m')
-    showCurrentPlayers(players)
-    print("\033[38;2;100;149;237m" + '='*178 + '\033[0m')
-    
-    print('='*178)
-    print("【请准备好身份牌并分发给每位对应玩家给每位玩家】")
-    print('='*178)
-    
-    DAY = "day"
-    NIGHT = "night"
-    day = 1
-    phase = "night"
-    game_over = False
-    
-    while(not game_over):
+class GameBoard1():
+    def __init__(self):
         
-        if phase == NIGHT:
-            nightPhase(day)
-            day += 1
-            phase = DAY
+        # 全局变量
+        self.DAY = "day_phase"
+        self.NIGHT = "night_phase"
+        self.day = 1
+        self.phase = self.NIGHT
+        self.game_over = False
+        self.players:list[Player] = None
+        self.alive_players:list[int] = None
+        
+        # 夜间行动变量
+        self.wild_child_model:int = -1
+        self.wolf_last_target:int = -1
+        self.witch_last_target:int = -1
+        self.witch_antidote:bool = True
+        self.poisson_used_tonight = False
+        self.hunter_shot = True
+        self.prophet_last_target:int = -1
+        self.prophet_options:list[int] = None
+        self.night_log:str = ""
+        
+    
+    def start(self):
+        print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+        
+        self.players = create_players_from_roles(currentRoles)
+        self.alive_players = [p.id for p in self.players if p.isAlive()]
+        self.prophet_options = [p.id for p in self.players if not p.role.name == "预言家"]
+        
+        print('='*178)
+        print("【请准备好身份牌并分发给每位对应玩家】")
+        print('='*178)
+        
+        while(not self.game_over):
             
-        elif phase == DAY:
-            dayPhase(day)
-            phase = NIGHT
-    
-    
-    print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
+            print("\033[38;2;100;149;237m" + '='*178 + '\033[0m')
+            showCurrentPlayers(self.players)
+            print("\033[38;2;100;149;237m" + '='*178 + '\033[0m')
+        
+            if self.phase == self.NIGHT:
+                self.nightPhase()
+                self.day += 1
+                self.phase = self.DAY
+                
+            elif self.phase == self.DAY:
+                self.dayPhase()
+                self.phase = self.NIGHT
+            
+        
+        print("\033[38;2;255;105;180m" + "="*178 + "\033[0m")
 
-def nightPhase(day:int):
-    pass
+    # 夜间行动
+    def nightPhase(self):
+        print("\033[38;2;135;206;250m" + '='*178 + '\033[0m')
+        # 第一夜环节: 混子行动
+        if self.day == 1:
+            wild_child_options = [p.id for p in self.players if not p.role.name == "混子"]
+            print('='*178)
+            print("【混子请睁眼】")
+            while not self.wild_child_model in wild_child_options:
+                wild_child_model_str = input("请选择榜样的号码: ")
+                try:
+                    self.wild_child_model = int(wild_child_model_str)
+                except ValueError:
+                    print("\033[31m" + "="*178 + "\033[0m")
+                    print("\033[31m" + "【注意！请选择以下有效数字: " + f"{wild_child_options}" "】\033[0m")
+                    print("\033[31m" + "="*178 + "\033[0m")
+                    continue
+                if not self.wild_child_model in wild_child_options:
+                    print("\033[31m" + "="*178 + "\033[0m")
+                    print("\033[31m" + "【注意！请选择以下有效数字: " + f"{wild_child_options}" "】\033[0m")
+                    print("\033[31m" + "="*178 + "\033[0m")
+                    continue
+            print("【混子请闭眼】")
+            print('='*178)
+        
+        # 狼人行动
+        print('='*178)
+        print("【狼人请睁眼】")
+        while not self.wolf_last_target in self.alive_players and not self.wolf_last_target == 0:
+            wolf_last_target_str = input("请选择要击杀的玩家号码，如果空刀请输入 0: ")
+            try:
+                self.wolf_last_target = int(wolf_last_target_str)
+            except ValueError:
+                print("\033[31m" + "="*178 + "\033[0m")
+                print("\033[31m" + "【注意！请选择以下有效数字: " + f"{self.alive_players}" "】\033[0m")
+                print("\033[31m" + "="*178 + "\033[0m")
+                continue
+            if self.wolf_last_target not in self.alive_players and not self.wolf_last_target == 0:
+                print("\033[31m" + "="*178 + "\033[0m")
+                print("\033[31m" + "【注意！请选择以下有效数字: " + f"{self.alive_players}" "】\033[0m")
+                print("\033[31m" + "="*178 + "\033[0m")
+                continue
+        print("【狼人请闭眼】")
+        print('='*178)
+        # 女巫行动
+        print('='*178)
+        print("【女巫请睁眼】")
+        self.poisson_used_tonight = False
+        if self.witch_antidote:
+            print("今晚死的玩家是: " + f"{self.wolf_last_target} 号, " + "你要救吗?")
+            use_antidote_str = input("(如果女巫选择了救， 请输入对应玩家号码。若没有救，请输入任意字节): ")
+            try:
+                use_antidote = int(use_antidote_str)
+            except ValueError:
+                use_antidote = 0
+            
+            if use_antidote == self.wolf_last_target:
+                self.wolf_last_target = -1
+                self.witch_antidote = False
+                self.poisson_used_tonight = True
+        else: 
+            print("今晚死的玩家是: " + "???号" + " 你要救吗?")
+            print("(女巫的解药已经使用，不需要告知击杀对象, 这只是流程。)")
+            input("按下 <ENTER> 继续")
+        
+        print("女巫你有一瓶毒药，请选择要使用的玩家号码。")
+        if self.poisson_used_tonight:
+            print("(女巫今夜已经用过解药， 无法再使用毒药，仅流程。)")
+            input("按下 <ENTER> 继续")
 
-def dayPhase(day):
-    pass
+        else:
+            while not self.witch_last_target in self.alive_players:
+                witch_last_target_str = input("(输入女巫毒药使用玩家号码): ")
+                try:
+                    self.witch_last_target = int(witch_last_target_str)
+                except ValueError:
+                    print("\033[31m" + "="*178 + "\033[0m")
+                    print("\033[31m" + "【注意！请选择以下有效数字: " + f"{self.alive_players}" "】\033[0m")
+                    print("\033[31m" + "="*178 + "\033[0m")
+                    continue
+                if not self.witch_last_target in self.alive_players:
+                    print("\033[31m" + "="*178 + "\033[0m")
+                    print("\033[31m" + "【注意！请选择以下有效数字: " + f"{self.alive_players}" "】\033[0m")
+                    print("\033[31m" + "="*178 + "\033[0m")
+                    continue
+        
+        print("【女巫请闭眼】")
+        print('='*178)
+        # 猎人睁眼确认开枪状态
+        print('='*178)
+        print("【猎人请睁眼】")
+        print("请确认你的开枪状态:")
+        for p in self.players:
+            if p.role.name == "猎人" and self.witch_last_target == p.id:
+                self.hunter_shot = False
+        if self.hunter_shot:
+            print("【 YES 】")
+            input("按下 <ENTER> 继续")
+        else: 
+            print("【 NO 】")
+            input("按下 <ENTER> 继续")
+
+        
+        print("【猎人请闭眼】")
+        print('='*178)
+        # 第一夜环节：白痴确认没看错身份
+        if self.day == 1:
+            print('='*178)
+            print("【白痴请睁眼】")
+            print("确认白痴身份")
+            input("按下 <ENTER> 继续")
+            print("【白痴请闭眼】")
+            print('='*178)
+        # 预言家环节
+        print('='*178)
+        print("【预言家请睁眼】")
+        
+        while not self.prophet_last_target in self.prophet_options:
+            prophet_last_target_str = input("请选择要查验的玩家号码: ")
+            try:
+                self.prophet_last_target = int(prophet_last_target_str)
+            except ValueError:
+                print("\033[31m" + "="*178 + "\033[0m")
+                print("\033[31m" + "【注意！请选择以下有效数字: " + f"{self.prophet_options}" "】\033[0m")
+                print("\033[31m" + "="*178 + "\033[0m")
+                continue
+            if self.prophet_last_target not in self.prophet_options:
+                print("\033[31m" + "="*178 + "\033[0m")
+                print("\033[31m" + "【注意！请选择以下有效数字: " + f"{self.prophet_options}" "】\033[0m")
+                print("\033[31m" + "="*178 + "\033[0m")
+                continue
+        for p in self.players:
+            if p.id == self.prophet_last_target:
+                if p.role.team == "神职" or p.role.team == "村民":
+                    print("他的身份是(请用相应手势示意，例：大拇指): 【好人】" )
+                else:
+                    print("他的身份是(请用相应手势示意，例：狼爪): 【狼人】" )
+        input("按下 <ENTER> 继续")
+        print("【预言家请闭眼】")
+        print('='*178)
+        
+        # 自动判定
+        self.night_log = f"\n【昨夜信息】\n"
+        dead_players:list[int] = []
+        
+        for p in self.players:
+            if p.id == self.witch_last_target:
+                p.alive = False
+                p.death_type = f"{p.death_type} 毒药"
+                dead_players.append(p.id)
+            if p.id == self.wolf_last_target:
+                p.alive = False
+                p.death_type = f"{p.death_type} 狼刀"
+                dead_players.append(p.id)
+        self.alive_players = [p.id for p in self.players if p.isAlive()]
+        
+        if dead_players == []:
+            self.night_log += ("昨夜, 平安夜。")
+        else:
+            random.shuffle(dead_players)
+            self.night_log += ("昨夜, " + f"{len(dead_players)}" + "死，死亡顺序不分先后" + 
+                                f"{dead_players[0]}号，{dead_players[1]}号 倒牌")
+        
+        if self.day == 1:
+            self.night_log += ("请发表遗言。\n")
+        else:
+            self.night_log += ("没有遗言。\n")
+            
+        print("\033[38;2;135;206;250m" + '='*178 + '\033[0m')
+        # 先不判断游戏进度
+        
+        
+        
+            
+            
+    # 白天阶段
+    def dayPhase(self):
+        
+        print("\033[38;2;255;215;0m" + '='*178 + '\033[0m')
+        print(self.night_log)
+        
+        # 测试阶段直接结束游戏
+        self.game_over = True
+        
+        print("\033[38;2;255;215;0m" + '='*178 + '\033[0m')
+
 
 def showCurrentPlayers(players:list[Player]):
     print()
